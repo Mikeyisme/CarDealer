31c9e454b7ba08390b7ba5eb1e0f54013c92099c
Mikeyisme <wmikal58@yahoo.com>
2022-03-30 20:26:31 -0500

    Commit

--- CarDealer/settings.py
+++ CarDealer/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cars',
     'dealers',
-    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -120,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static')
+            os.path.join(BASE_DIR, 'static_in_project')
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
