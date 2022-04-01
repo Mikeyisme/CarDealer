54c2e8fc08766586fcf4a82d3cee539d77e6e25c
Mikeyisme <wmikal58@yahoo.com>
2022-04-01 17:24:28 -0500

    Commit

--- CarDealer/settings/base.py
+++ CarDealer/settings/base.py
@@ -76,6 +76,7 @@ load_dotenv(find_dotenv())
 
 DATABASES = {'default': dj_database_url.config(default='sqlite3:///db.sqlite', conn_max_age=600)}
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
