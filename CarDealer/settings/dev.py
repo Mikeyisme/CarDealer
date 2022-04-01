767ed44087cdb8352194422362b167fe7037eb53
Mikeyisme <wmikal58@yahoo.com>
2022-04-01 17:28:19 -0500

    Commit

--- CarDealer/settings/dev.py
+++ CarDealer/settings/dev.py
@@ -6,10 +6,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
