833b3cdacf7514918180847dcded810282f7f389
Mikal Williams <98860058+Mikeyisme@users.noreply.github.com>
2022-04-18 19:55:33 -0500

    Update base.py

--- CarDealer/settings/base.py
+++ CarDealer/settings/base.py
@@ -117,8 +117,3 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'groundshaka317@gmail.com'
-EMAIL_HOST_PASSWORD = 'Imtocool123'
-EMAIL_USE_TLS =  True 
